5d3fc38c01620cbcc387c7292478ca2a06d246f8
grandballoon <luke.sovern.conley@gmail.com>
2018-06-27 13:50:16 -0400

    count sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
-  end
+    word_array = self.split
+    final_words = word_array.select do |word|
+      word.sentence? || word.question? || word.exclamation?
+    end
+  final_words.length
+end
+ 
 end
