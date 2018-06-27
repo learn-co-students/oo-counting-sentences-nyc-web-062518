d673ba1dac875831c81891c9050cda67f531478d
Daniel Ran <danielr1735@gmail.com>
2018-06-27 20:43:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,29 +14,29 @@ class String
     self.end_with?("!")
   end
 
-  def count_sentences
-    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
-  end
-
   # def count_sentences
-  #   char_array = self.split('')
-  #   i = 0
-  #   count = 0
-  #
-  #   while i < (char_array.length - 1)
-  #     case char_array[i] + char_array[i+1]
-  #       when ". ", "! ", "? "
-  #         count += 1
-  #     end
-  #     i += 1
-  #   end
-  #
-  #   case char_array[-1]
-  #     when ".", "!", "?"
-  #       count += 1
-  #   end
-  #   count
+  #   self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   # end
+
+  def count_sentences
+    char_array = self.split('')
+    i = 0
+    count = 0
+
+    while i < (char_array.length - 1)
+      case char_array[i] + char_array[i+1]
+        when ". ", "! ", "? "
+          count += 1
+      end
+      i += 1
+    end
+
+    case char_array[-1]
+      when ".", "!", "?"
+        count += 1
+    end
+    count
+  end
 
 
 end
