040954090685ad24b154fd0eb79129f9a73e8e46
Daniel Ran <danielr1735@gmail.com>
2018-06-27 20:30:13 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    char_array = self.split('')
+    i = 0
+    count = 0
 
+    while i < (char_array.length - 1)
+      case char_array[i] + char_array[i+1]
+        when ". ", "! ", "? "
+          count += 1
+      end
+      i +=1
+
+    end
+
+    case char_array[-1]
+      when ".", "!", "?"
+        count += 1
+    end
+    count
   end
+
+
 end
+
+# "Hello, my name is Daniel! Yes!".count_sentences
