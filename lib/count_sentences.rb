5b2c1475a94dc2abfdfebababff593df1711c820
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-06-27 22:54:26 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,27 @@ require 'pry'
 
 class String
 
+  attr_accessor :sentence
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_count = 0
+    array = self.split(' ')
+    array.each do |word|
+      sentence_count += 1 if word.end_with?("!") || word.end_with?(".") || word.end_with?("?") || word.end_with?("!!") || word.end_with?("...")
+    end
+    sentence_count
   end
+
 end
