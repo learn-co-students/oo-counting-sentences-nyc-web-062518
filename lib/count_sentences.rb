760e28d92782f12b4a01c017cefce52ff9e42a72
Lillian Zhang <lilyzhangyun@gmail.com>
2018-07-02 17:25:59 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".")
   end
 
   def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
 
   end
 end
