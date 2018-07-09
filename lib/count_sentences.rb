310f0c922a4e261cbf4abf9cf8b6d28922f1912e
Ena Anicama <ena.v825@gmail.com>
2018-07-09 17:22:05 -0400

    complete lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
 require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+      # binding.pry
+    # self.split(/[\.\!\?\...]/).count
+    # returns num of sentences in a string
+    self.split(/[.?!]+/).grep(/\S/).count
   end
+
 end
